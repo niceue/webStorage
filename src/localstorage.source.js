b29577e93c7ea05a4b1fb7dd995b55b224672dc1
niceue <zj86@foxmail.com>
2012-03-28 15:28:07 +0800

    build

--- src/localstorage.source.js
+++ src/localstorage.source.js
@@ -7,43 +7,43 @@
         // userData IE5+ (http://msdn.microsoft.com/en-us/library/ms531424.aspx)
         if(d.documentElement.addBehavior){
             var e = d.createElement("meta"), s = "localStorage";
-			e.addBehavior("#default#userdata");
-			d.getElementsByTagName("head")[0].appendChild(e);
-			var w.localStorage = {
-					length: 0,
-					key: function(n){
-						return (n>=0 && n<this.length) ? _a()[n].nodeName : null
-					},
-					setItem: function(key , val){
-						e.load(s);
-						e.setAttribute(key , val);
-						e.save(s);
-						_l();
-					},
-					getItem: function(key){
-						e.load(s);
-						return e.getAttribute(key);
-					},
-					removeItem: function(key){
-						e.load(s);
-						e.removeAttribute(key);
-						e.save(s);
-						_l();
-					},
-					clear: function(){
-						e.load(s);
-						var a, i = 0;
-						while (a = _a()[i++]) e.removeAttribute(a.nodeName);
-						e.save(s);
-						this.length=0;
-					}
-				},
+            e.addBehavior("#default#userdata");
+            d.getElementsByTagName("head")[0].appendChild(e);
+            var w.localStorage = {
+                    length: 0,
+                    key: function(n){
+                        return (n>=0 && n<this.length) ? _a()[n].nodeName : null
+                    },
+                    setItem: function(key , val){
+                        e.load(s);
+                        e.setAttribute(key , val);
+                        e.save(s);
+                        _l();
+                    },
+                    getItem: function(key){
+                        e.load(s);
+                        return e.getAttribute(key);
+                    },
+                    removeItem: function(key){
+                        e.load(s);
+                        e.removeAttribute(key);
+                        e.save(s);
+                        _l();
+                    },
+                    clear: function(){
+                        e.load(s);
+                        var a, i = 0;
+                        while (a = _a()[i++]) e.removeAttribute(a.nodeName);
+                        e.save(s);
+                        this.length=0;
+                    }
+                },
                 _a = function(){
                     return e.XMLDocument.documentElement.attributes
                 },
-				_l = function(){
-					w.localStorage.length = _a().length
-				};
+                _l = function(){
+                    w.localStorage.length = _a().length
+                };
             e.load(s);
             _l();
 
